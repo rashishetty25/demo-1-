4b32df648c903a8f2914da9bebcc613b14dc37d3
RS <rashishetty25@gmail.com>
2024-02-26 14:10:02 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,10 +1,8 @@
 import random
 
-# Generate random numbers
 num1 = random.randint(1, 100)
 num2 = random.randint(1, 100)
 
-# Add the numbers
 result = num1 + num2
 
 # Print the numbers and the result
